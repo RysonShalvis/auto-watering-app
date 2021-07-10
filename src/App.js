f106e6d472237e16bc8b8a0afbce023b932d90f3
RysonShalvis <rysonshalvis@gmail.com>
2021-07-09 19:55:47 -0600

    add login and user components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import React, {useState} from 'react';
+import User from './User';
 
 function App() {
-  return <h1>auto watering app</h1>
+
+  // const [count, setCount] = useState(0);
+  return (
+    <Router>
+      <Route exact path="/" component={User} />
+    </Router>
+    )
+
 }
 
 export default App;
